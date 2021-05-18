c73be9b983e95bcb4928c984f554103659a0db35
Aric Renzo <aricrenzo@gmail.com>
2021-05-17 20:00:57 -0400

    adding changes to support auto cloudflare updates

--- main.py
+++ main.py
@@ -64,6 +64,39 @@ bot = commands.Bot(
     case_insensitive=True
 )
 
+#Update CloudFlare - Example: https://github.com/cloudflare/python-cloudflare/blob/master/examples/example_update_dynamic_dns.py#L77
+def update_cloudflare(public_ip):
+    #CloudFlare Client
+    cf_token = config.get("cloudflare").get("cloudflare_access_token")
+    cf = CloudFlare.CloudFlare(token=cf_token)
+    
+    #Parameters
+    cf_zone_id = config.get("cloudflare").get("dns_zone_id")
+    cf_endpoint = config.get("cloudflare").get("endpoint_name")
+
+    params = {'name':cf_endpoint, 'match':'all'}
+    main_endpoint = cf.zones.dns_records.get(cf_zone_id, params=params)
+    main_endpoint_id = main_endpoint[0]['id']
+
+    #Update the CF entry
+    dns_record = {
+        'name': cf_endpoint,
+        'type': "A",
+        'content': public_ip,
+        'proxied': True,
+    }
+
+    try:
+        dns_record = cf.zones.dns_records.put(cf_zone_id, main_endpoint_id, data=dns_record)
+    except CloudFlare.exceptions.CloudFlareAPIError as e:
+        exit('/zones.dns_records.put %s - %d %s - api call failed' % (cf_endpoint, e, e))
+    print("CloudFlare UPDATED!")
+
+    #Delay for 10 seconds while cloudflare updates
+    time.sleep(10)
+    return cf_endpoint
+
+
 @bot.check
 async def echo_message(ctx):
     print(ctx)
@@ -90,13 +123,13 @@ async def status(ctx, realm_name="foundry"):
     else:
         await ctx.send("I do not know if that realm. Maybe its known by some other name?")
         return
-
+    cloudflare_endpoint = config.get("cloudflare").get("endpoint_name")
     realm_instance = ec2.Instance(realm_instance_id)
     status = realm_instance.state["Name"]
     public_ip_address = realm_instance.public_ip_address
 
     if status == "running":
-        reply_message = f"The gateway to the {realm_name} realm is currently open at https://{public_ip_address}. Enjoy."
+        reply_message = f"The gateway to the {realm_name} realm is currently open at https://{cloudflare_endpoint} -- https://{public_ip_address}. Enjoy."
 
     else:
         reply_message = f"The gateway to the {realm_name} realm is closed currently."
@@ -125,7 +158,7 @@ async def start(ctx, realm_name="foundry"):
         realm_instance.start()
         
         operation_time = 0
-        poll_frequency_ms = 5000 # 5 second polling ineterval
+        poll_frequency_ms = 5000 # 5 second polling interval
         operation_timeout = 300000
         while status != "running":
             print("Checking EC2 instance status...")
@@ -141,8 +174,9 @@ async def start(ctx, realm_name="foundry"):
         
         if status == "running":
             public_ip_address = realm_instance.public_ip_address
+            cloudflare_endpoint = update_cloudflare(public_ip_address)
             await ctx.send(f"The {realm_name} realm is online. You may proceed with care to the realm. Stay safe. I'll be watching for your return.")
-            await ctx.send(f"https://{public_ip_address}")
+            await ctx.send(f"https://{cloudflare_endpoint} -- https://{public_ip_address}")
 
             now = datetime.now()
             stop_after = now + timedelta(hours=4)
